1316bc242b5cb3d58fecfd27778a9e3830feed44
ethen.zhan <ethen.zhan@chroma.com.tw>
2023-08-30 12:00:00 +0800

    create

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MobileNetComponent } from './tensorFlow/mobile-net/mobile-net.component';
+import { TimeLineComponent } from './time-line/time-line.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  {
+    path: 'mobileNet',
+    component: MobileNetComponent
+  },
+  {
+    path: 'timeLine',
+    component: TimeLineComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
